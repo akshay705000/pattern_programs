b8b641bd54649fcd281c4b86d0929750107030a8
akshay705000 <36586885+akshay705000@users.noreply.github.com>
2018-07-21 16:20:06 +0530

    Create right_angled_triangle.py

---
+++ right_angled_triangle.py
@@ -0,0 +1,15 @@
+a=int(input("enter height of right angled triangle"))
+for x in range(1,a+1):
+ for y in range(1,x+1):
+  print("*",end="")
+ print()
+
+
+
+#    *
+#    **
+#    ***
+#    ****
+#    *****
+#    ******
+#    *******
